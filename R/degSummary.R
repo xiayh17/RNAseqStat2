8319ec2e99032c2212a44269f1a784d7f93fbcb4
xiayh17 <xiayh17@gmail.com>
2022-02-22 10:07:21 +0800

    :zap: update deg module

---
+++ R/degSummary.R
@@ -0,0 +1,85 @@
+#' @export
+degSummary <- function(obj, dir = ".", prefix = "2-runDEG",
+                       PointVolcanoParam = list(gene = 10,light = NULL,label_light = TRUE,
+                                                light_color = "#24ac56",
+                                                light_label_color = "#24ac56",
+                                                expend = c(0.12, 0.12))) {
+
+  if (!fs::dir_exists(dir)) {
+    fs::dir_create(dir)
+  }
+
+  test <- deg_here(obj)
+  ok <- names(test)[which(test == TRUE)]
+  main <- setdiff(ok,"merge")
+  # merge_data <- intersect(ok,"merge")
+
+  if(test["merge"]) {
+
+    dat <- dataDEG(obj,which = "merge")
+    csv_name = glue('{dir}/{prefix}_merge_results.csv')
+    write.csv(dat,file = csv_name)
+    ui_done(glue("merge DEG results in csv format is stored in {usethis::ui_path(csv_name)}"))
+
+    geneSymbol_list <- hyper_GS(obj,"merge",type = "SYMBOL")
+    lapply(seq_along(geneSymbol_list), function(x){
+
+      file_name <- glue("{dir}/{prefix}_{names(geneSymbol_list)[x]}_merge_Gene.txt")
+      writeLines(geneSymbol_list[[x]],con = file_name)
+      ui_done(glue("merge {names(geneSymbol_list)[x]} Gene in txt format is stored in {usethis::ui_path(file_name)}"))
+
+    })
+
+  }
+
+  ## plot and export data
+  if(length(main)>=1){
+
+    for (i in main) {
+
+      which = i
+
+      geneSymbol_list <- hyper_GS(object = obj,which = which,type = "SYMBOL")
+      lapply(seq_along(geneSymbol_list), function(x){
+
+        file_name <- glue("{dir}/{prefix}_{names(geneSymbol_list)[x]}_{which}_Gene.txt")
+        writeLines(geneSymbol_list[[x]],con = file_name)
+        ui_done(glue("{which} {names(geneSymbol_list)[x]} Gene in txt format is stored in {usethis::ui_path(file_name)}"))
+
+      })
+
+      dat <- dataDEG(obj,which = which)
+      csv_name = glue('{dir}/{prefix}_{which}_results.csv')
+      write.csv(dat,file = csv_name)
+      ui_done(glue("{which} DEG results in csv format is stored in {usethis::ui_path(csv_name)}"))
+
+      volcano_plot <-  do.call("PointVolcano",c(list(obj = obj,which = i), PointVolcanoParam))
+      volcano_file = glue("{dir}/{prefix}_{which}_volcano.pdf")
+      ggsave(volcano_plot,filename = volcano_file, width = 1600,height = 1600,units = "px",limitsize = FALSE,device = cairo_pdf)
+      ui_done(glue("{which} volcano results were store in {ui_path(volcano_file)}."))
+
+    }
+
+  } else {
+    ui_info("None available results of DEG.")
+  }
+
+}
+
+dataDEG <- function(obj,which) {
+
+  if (which == "limma") {
+    deg_data = limma_res(obj)
+  } else if (which == "edgeR") {
+    deg_data = edgeR_res(obj)
+  } else if (which == "DESeq2") {
+    deg_data = DESeq2_res(obj)
+  } else if (which == "merge") {
+    deg_data = merge_res(obj)
+  } else {
+    ui_stop("{ui_code('which')} should be one of {ui_value('limma, edgeR, DESeq2')}")
+  }
+
+  return(deg_data)
+
+}
--- R/gseResolve.R
+++ R/gseResolve.R
@@ -1,24 +1,61 @@
 #' @export
-gseResolve <- function(object) {
+gseResolve <- function(object, GO = FALSE, KEGG = TRUE) {
 
   ## 获取GeneList
   test <- deg_here(object)
   ok <- names(test)[which(test == TRUE)] ## 取有效数据
 
-  gseKEGG_GeneSets = list()
-  for (i in ok) {
-    gseKEGG_GeneSets[[i]] <- suppressWarnings(GSEA_GS(object,which = i,type = "ENTREZID"))
+  if(KEGG) {
+
+    usethis::ui_info("GSE KEGG analysis Start. This process will take a few minutes.")
+
+    keggParams <- gseKEGGparam(object)
+    gseKEGG_GeneSets = list()
+
+    if("TERM2GENE" %in% names(keggParams)){
+      for (i in ok) {
+        gseKEGG_GeneSets[[i]] <- suppressWarnings(GSEA_GS(object,which = i,type = "SYMBOL"))
+      }
+    } else {
+      for (i in ok) {
+        gseKEGG_GeneSets[[i]] <- suppressWarnings(GSEA_GS(object,which = i,type = "ENTREZID"))
+      }
+    }
+
+    ## 富集分析
+    gseKEGG_res <- gse_keggResolve(geneSet_list = gseKEGG_GeneSets,keggParams = keggParams)
+
+  } else {
+
+    gseKEGG_res <- NULL
+
   }
 
-  ## 富集分析
-  keggParams <- gseKEGGparam(object)
+  if(GO) {
 
-  gseKEGG_res <- gse_keggResolve(geneSet_list = gseKEGG_GeneSets,keggParams = keggParams)
+    usethis::ui_info("GSE GO analysis Start. This process will take a few minutes.")
+
+    goParams <- gseGOparam(object)
+    gseGO_GeneSets = list()
+
+    for (i in ok) {
+      gseGO_GeneSets[[i]] <- suppressWarnings(GSEA_GS(object,which = i,type = "SYMBOL"))
+    }
+
+    ## 富集分析
+    gseGO_res <- gse_goResolve(geneSet_list = gseGO_GeneSets,goParams = goParams)
+
+  } else {
+
+    gseGO_res = NULL
+
+  }
 
 
   ## 保存结果
   tmp <- gseRes(object)
   tmp["gseKEGG_res"] <- list(gseKEGG_res)
+  tmp["gseGO_res"] <- list(gseGO_res)
   gseRes(object) <- tmp
 
   return(object)
@@ -28,49 +65,82 @@ gseResolve <- function(object) {
 #' @export
 gse_keggResolve <- function(...,geneSet_list,keggParams) {
 
-  usethis::ui_info("Enrich KEGG analysis Start. This process will take a few minutes.")
+  keggres <- lapply(seq_along(geneSet_list), function(x){
 
-  keggres <- lapply(geneSet_list, function(x){
+    gene = geneSet_list[[x]]
 
     tryCatch(
       expr = {
-        gse_keggCore(gene=x,keggParams=keggParams)
+        gseCore(gene=gene,fparams=keggParams,f = "gseKEGG")
       },
       error = function(e){
         usethis::ui_oops("Something wrong occured. try again.")
-        gse_keggCore(gene=x,keggParams=keggParams)
+        gseCore(gene=gene,fparams=keggParams,f = "gseKEGG")
       },
       finally = {
-        usethis::ui_line("Enrich KEGG analysis done")
+        usethis::ui_line("GSE KEGG {names(geneSet_list)[x]} analysis done")
       }
     )
 
   })
 
+  names(keggres) <- names(geneSet_list)
+
+  return(keggres)
+
 }
 
-gse_keggCore <- function(...,keggParams){
+#' @importFrom clusterProfiler gseGO
+#' @export
+gse_goResolve <- function(...,geneSet_list,goParams) {
+
+  gores <- lapply(seq_along(geneSet_list), function(x){
+
+    gene = geneSet_list[[x]]
+
+    tryCatch(
+      expr = {
+        gseCore(gene=gene,fparams=goParams,f = "gseGO")
+      },
+      error = function(e){
+        usethis::ui_oops("Something wrong occured. try again.")
+        gseCore(gene=gene,fparams=goParams,f = "gseGO")
+      },
+      finally = {
+        usethis::ui_line("GSE GO {names(geneSet_list)[x]} analysis done")
+      }
+    )
+
+  })
+
+  names(gores) <- names(geneSet_list)
+
+  return(gores)
+
+}
+
+#' @importFrom clusterProfiler gseKEGG GSEA
+gseCore <- function(..., fparams, f = "gseKEGG") {
 
   params <- list(...)
-  keggParams <- modifyList(params, keggParams)
-  kegg_core <- suppressMessages(do.call("gseKEGG", modifyList(
-    list(),
-    keggParams)
-  ))
+  fparams <- modifyList(params, fparams)
 
-  return(kegg_core)
+  if("TERM2GENE" %in% names(fparams)){
+
+    core <- suppressMessages(do.call("GSEA", modifyList(
+      list(),
+      fparams)
+    ))
+
+  } else {
+
+    core <- suppressMessages(do.call(f, modifyList(
+      list(),
+      fparams)
+    ))
+
+  }
+
+  return(core)
 
 }
-
-# gse_goCore <- function(...,goParams){
-#
-#   params <- list(...)
-#   goParams <- modifyList(params, goParams)
-#   go_core <- suppressMessages(do.call("gseGO", modifyList(
-#     list(),
-#     goParams)
-#   ))
-#
-#   return(go_core)
-#
-# }
